787eb0faf83754857af4efec6744752b605919c3
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-03-11 09:29:19 +0100

    Form added

--- src/components/views/CartPage.jsx
+++ src/components/views/CartPage.jsx
@@ -21,24 +21,51 @@ const CartPage = () => {
         <button className='deleteBtn' onClick={removeAllProducts}>Delete all products</button>
       </div>
       {purchasedProducts.length > 0 ? (
-        <table>
-          <tbody>
-            {purchasedProducts.map(product => (
-              <tr key={product.id}>
-                <td className='imgPurchasedProduct'>
-                  <img src={product.image} alt={product.title} />
-                </td>
-                <td className='detailsPurchasedProduct'>{product.title}</td>
-                <td className='pricePurchasedProduct'>${product.price}</td>
-                <td>
-                  <FaTimes className='x' onClick={() => removeSingleProduct(product.id)} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div>
+          <table>
+            <tbody>
+              {purchasedProducts.map(product => (
+                <tr key={product.id}>
+                  <td className='imgPurchasedProduct'>
+                    <img src={product.image} alt={product.title} />
+                  </td>
+                  <td className='detailsPurchasedProduct'>{product.title}</td>
+                  <td className='pricePurchasedProduct'>${product.price}</td>
+                  <td>
+                    <FaTimes className='x' onClick={() => removeSingleProduct(product.id)} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <form>
+            <fieldset onSubmit={}>
+              <h3>Personal info</h3>
+              <label htmlFor="Name">Name:</label>
+              <input type="text" />
+              <label htmlFor="Name">Surname:</label>
+              <input type="text" /> <br />
+              <label htmlFor="Mobile">Mobile:</label>
+              <input type="number"/>
+              <h3>Address</h3>
+              <label htmlFor="Country">Country:</label>
+              <input type="text"/> <br />
+              <label htmlFor="City">City:</label>
+              <input type="text"/> <br />
+              <label htmlFor="Country">Country:</label>
+              <input type="text"/> <br />
+              <label htmlFor="Number">Number:</label>
+              <input type="text"/> <br />
+
+                <button></button>
+            </fieldset>
+          </form>
+        </div>
       ) : (
-        <p>Your cart is empty</p>
+        <div>
+          <p>Your cart is empty</p>
+          <button onClick={() =>{ window.open('/products')}}>Back on products</button>
+        </div>
       )}
     </div>
   );
